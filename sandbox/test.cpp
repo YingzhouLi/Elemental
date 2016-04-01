1d533dd68b99af93e34372300a73308d2ca0c72d
YingzhouLi <lyzh588@gmail.com>
2016-04-01 14:45:58 -0700

    Added an example with error in sandbox

--- sandbox/test.cpp
+++ sandbox/test.cpp
@@ -12,8 +12,48 @@ int main( int argc, char* argv[] )
         ProcessInput();
 
         DistMatrix<double> A;
-        Uniform( A, m, n );
-        Print( A, "A" );
+        Zeros( A, m, n );
+        for( int i=0; i < n; ++i )
+            A.Set(i,i,double(i));
+        DistMatrix<double> Ainv;
+        Zeros( Ainv, m, n );
+        for( int i=0; i < n; ++i )
+            Ainv.Set(i,i,1.0/double(i));
+        auto applyA = 
+            [&]( double alpha, const DistMultiVec<double>& X,
+                 double beta, DistMultiVec<double>& Y )
+            {
+                DistMatrix<double> XMat;
+                Copy( X, XMat );
+                DistMatrix<double> YMat;
+                Copy( Y, YMat );
+                Gemm( NORMAL, NORMAL, alpha, A, XMat, beta, YMat );    
+                Copy( YMat, Y );
+            };
+        auto precond = 
+            [&]( DistMultiVec<double>& W )
+            {
+                /*
+                DistMatrix<double> WCopy;
+                Copy( W, WCopy );
+                DistMatrix<double> WMat;
+                Copy( W, WMat );
+                Gemm( NORMAL, NORMAL, double(1), Ainv, WCopy, double(0), WMat );
+                Copy( WMat, W );
+                */
+            };
+
+        DistMatrix<double> bb;
+        Uniform( bb, n, 1 );
+        DistMultiVec<double> b;
+        Copy( bb, b );
+        double relTol = 1e-12;
+        int restart = 30;
+        int maxIts = floor(m/restart);
+        bool progress = true;
+
+        int iter = LGMRES( applyA, precond, b, relTol,
+                           restart, maxIts, progress );
     }
     catch( std::exception& e ) { ReportException(e); }
 
