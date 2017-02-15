ad60dc0a62a294f939f5ad11f7aa61ade10bb9c6
YingzhouLi <yingzhouli0417@gmail.com>
2017-02-15 15:59:49 -0800

    Updated ICMEShare*.cmake

--- cmake/toolchains/ICMEShare-gnu-mpich2.cmake
+++ cmake/toolchains/ICMEShare-gnu-mpich2.cmake
@@ -1,13 +1,11 @@
+set(CMAKE_C_COMPILER /usr/include/gcc6/bin/gcc)
+set(CMAKE_CXX_COMPILER /usr/include/gcc6/bin/g++)
+set(CMAKE_Fortran_COMPILER /usr/include/gcc6/bin/gfortran)
+
 # The MPI wrappers for the C and C++ compilers
 set(MPI_COMPILER_DIR /usr/lib64/mpich2/bin/)
 set(MPI_C_COMPILER       ${MPI_COMPILER_DIR}/mpicc)
 set(MPI_CXX_COMPILER     ${MPI_COMPILER_DIR}/mpicxx)
 set(MPI_Fortran_COMPILER ${MPI_COMPILER_DIR}/mpif90)
 
-if(CMAKE_BUILD_TYPE MATCHES Debug)
-  set(CXX_FLAGS "-g")
-else()
-  set(CXX_FLAGS "-O3")
-endif()
-
-set(OpenMP_CXX_FLAGS "-fopenmp")
+set(GFORTRAN_LIB /usr/include/gcc6/lib64/libgfortran.so)
